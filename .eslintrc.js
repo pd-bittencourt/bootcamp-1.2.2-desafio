264f47dbe989f2b1510f0e8a740e35d644fba610
Pedro <pedrodmbc@gmail.com>
2019-10-05 21:47:05 -0300

    chore: update eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,23 @@
 module.exports = {
-  extends: ["airbnb-base", "prettier"],
-  plugins: ["prettier"],
   env: {
     es6: true,
-    node: true
+    node: true,
   },
-  extends: ["airbnb-base"],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error", // todos os erros do prettier retornar erro no eslint
-    "class-methods-use-this": "off", // express tem métodos sem this
-    "no-param-reassign": "off", // para o sequelize
-    camelcase: "off", // n todas as variaveis definidas com camelcase
-    "no-unused-vars": ["error", { argsIgnorePattern: "next" }]
-  }
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
 };
