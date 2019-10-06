2eb59754537b866b14df1a9bf0e8890e858bc4b4
Pedro <pedrodmbc@gmail.com>
2019-10-05 22:34:33 -0300

    chore: create model loader

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Pedro Bittencourt',
+    email: 'pedro1@email.com',
+    password_hash: '123123',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
